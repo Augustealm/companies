80f56f66c44f49befbbbcce007f0cbbff775613c
Auguste <Auguste.alm@gmail.com>
2020-05-25 14:19:11 +0300

    pakeitimai

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 use App\Company;
 use Illuminate\Http\Request;
+use Symfony\Component\Console\Input\Input;
 
 class CompanyController extends Controller
 {
-    public function index(){
-        return view('pages.home');
+    public function __construct()
+    {
+        $this->middleware('auth', ['except'=>['index', 'all', 'info', 'search']]);
+    }
+
+    public function index(Company $company){
+        return view('pages.home', ['company'=>$company]);
     }
 
     public function add(){
@@ -15,26 +21,28 @@ class CompanyController extends Controller
     }
 
     public function edit() {
-        $companies = Company::all();
+        $companies = Company::paginate(6);
         return view('pages.edit', compact('companies'));
     }
 
     public function editCompany(Company $company){
         return view('pages.edit-company', ['company'=>$company]);
     }
+
     public function storeUpdate(Request $request, Company $company){
         $request->validate([
             'pavadinimas'=> ['required', 'max:50'],
             'kodas'=>['required', 'max:15'],
             'pvm_kodas'=>['required', 'max:20'],
-            'adresas'=> ['required', 'max:50'],
-            'telefonas'=>['required', 'max:15'],
-            'el_pastas'=>['required', 'max:20'],
-            'veikla'=>['required', 'max:40'],
-            'vadovas'=>['required', 'max:20']
+            'adresas'=> ['required', 'max:70'],
+            'telefonas'=>['required', 'max:12'],
+            'el_pastas'=>['required', 'regex:/(.+)@(.+)\.(.+)/i'],
+            'veikla'=>['required', 'max:70'],
+            'vadovas'=>['required', 'max:40']
         ]);
+
         Company::where ('id', $company->id)->update($request->only(['pavadinimas', 'kodas', 'pvm_kodas', 'adresas', 'telefonas', 'el_pastas', 'veikla', 'vadovas']));
-        return redirect('/');
+        return redirect('/edit');
     }
 
     public function store(Request $request){
@@ -42,11 +50,11 @@ class CompanyController extends Controller
             'pavadinimas'=> ['required', 'max:50'],
             'kodas'=>['required', 'max:15'],
             'pvm_kodas'=>['required', 'max:20'],
-            'adresas'=> ['required', 'max:50'],
-            'telefonas'=>['required', 'max:15'],
-            'el_pastas'=>['required', 'max:20'],
-            'veikla'=>['required', 'max:40'],
-            'vadovas'=>['required', 'max:20']
+            'adresas'=> ['required', 'max:70'],
+            'telefonas'=>['required', 'max:12'],
+            'el_pastas'=>['required', 'regex:/(.+)@(.+)\.(.+)/i'],
+            'veikla'=>['required', 'max:70'],
+            'vadovas'=>['required', 'max:40']
         ]);
 
         Company::create([
@@ -60,16 +68,48 @@ class CompanyController extends Controller
             'vadovas'=> request('vadovas')
         ]);
 
+        session()->flash('notif', 'Informacija sėkmingai išsaugota!');
+        return redirect('/add-company');
+
     }
 
     public function all() {
-        $companies = Company::all();
+        $companies = Company::paginate(6);
         return view('pages.all-companies', compact('companies'));
     }
-    public function info() {
-        $companies = Company::all();
-        return view('pages.all-info', compact('companies'));
+    public function info(Company $company){
+        return view('pages.all-info', ['company'=>$company]);
     }
 
+
+    public function delete(Company $company){
+        $company->delete();
+        return view('pages.home');
+
+    }
+
+    public function search(Request $request){
+        $request->validate([
+            'category' => 'required|min:3'
+        ]);
+
+        $category = $request->input('category');
+
+        $companies = Company::where('pavadinimas','like',  '%' . $category . '%')->orWhere('kodas','like',  '%' . $category . '%')-> get();
+  //   $companiesCount = $companies->count();
+   //    $request->validate([
+     //    $companiesCount => 'required|min:1'
+     //  ]);
+
+
+        return view('pages.search-results', compact('companies'));
+
+    }
+    public function logout () {
+        //logout user
+        auth()->logout();
+        // redirect to homepage
+        return redirect('/');
+    }
 
 }
